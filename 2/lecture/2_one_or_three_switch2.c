77ff3fa47c1ae30a28b595631fc33c7de3093074
Michael Kirkedal Thomsen <kirkedal@acm.org>
2016-09-07 09:22:29 +0200

    file 3

---
+++ 2/lecture/2_one_or_three_switch2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  switch (argc) {
+    case 2:
+    case 4: 
+      exit(EXIT_SUCCESS);
+      break;
+    default:
+      exit(EXIT_FAILURE);
+      break;
+  }
+}
